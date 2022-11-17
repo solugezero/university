30888d091a550e0a85cb085ff45020fac5914c2b
solugezero <solugezero@mail.ru>
2022-11-17 19:43:14 +0300

    upd

--- components/organisms/header.vue
+++ components/organisms/header.vue
@@ -1,49 +1,48 @@
 <template>
   <div class="header">
     <div class="header-wrapper content-box">
-        <NuxtLink to="/"><img src="@/assets/imgs/logo.png" alt="" class="header-wrapper__logo"></NuxtLink>
-        <div class="header-wrapper_buttons">
-            <NuxtLink to="/referats"><ui-button>Рефераты</ui-button></NuxtLink>
-            <NuxtLink to="/lab"><ui-button>Лабораторные работы</ui-button></NuxtLink>
-            <NuxtLink to="/presentation"><ui-button>Презентации и курсы</ui-button></NuxtLink>
-        </div>
+      <NuxtLink to="/"><img src="@/assets/imgs/logo.png" alt="" class="header-wrapper__logo" /></NuxtLink>
+      <div class="header-wrapper_buttons">
+        <NuxtLink to="/referats"><ui-button>Рефераты</ui-button></NuxtLink>
+        <NuxtLink to="/lab"><ui-button>Лабораторные работы</ui-button></NuxtLink>
+        <NuxtLink to="/presentation"><ui-button>Презентации</ui-button></NuxtLink>
+        <NuxtLink to="/course"><ui-button>Курсы</ui-button></NuxtLink>
+      </div>
     </div>
   </div>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
 
 <style scoped lang="scss">
 .header {
-    width: 100%;
-    height: 80px;
+  width: 100%;
+  height: 80px;
+  display: flex;
+  background: rgba(255, 255, 255, 0.05);
+  box-shadow: 0 8px 20px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(3px);
+  -webkit-backdrop-filter: blur(3px);
+  border: 1px solid rgba(255, 255, 255, 0.18);
+
+  &-wrapper {
     display: flex;
-    background: rgba( 255, 255, 255, 0.05 );
-    box-shadow: 0 8px 20px 0 rgba( 31, 38, 135, 0.37 );
-    backdrop-filter: blur( 3px );
-    -webkit-backdrop-filter: blur( 3px );
-    border: 1px solid rgba( 255, 255, 255, 0.18 );
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: row;
 
-    &-wrapper {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        flex-direction: row;
-
-        &__logo {
-            width: 40px;
-            height: 40px;
-        }
-
-        &_buttons {
-            display: flex;
-            width: auto;
-            height: auto;
-            flex-direction: row;
-            gap: 10px;
-        }
+    &__logo {
+      width: 40px;
+      height: 40px;
     }
+
+    &_buttons {
+      display: flex;
+      width: auto;
+      height: auto;
+      flex-direction: row;
+      gap: 10px;
+    }
+  }
 }
 </style>
---
+++ pages/course/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="course">
+    <div class="content-box">
+      <OrganismsCreateForm>
+        <template v-slot:activeButton>
+          <ui-button>Добавить работу</ui-button>
+        </template>
+      </OrganismsCreateForm>
+      <div class="course_wrapper">
+        <p class="course_wrapper__title">Список курсов</p>
+        <div class="course_list"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+let data = ref({
+  referats: [
+    {
+      author: "",
+      description: "",
+      datePublication: "",
+    },
+  ],
+});
+</script>
+
+<style lang="scss" scoped>
+.course {
+  display: flex;
+  width: 100%;
+  height: auto;
+  // background: rgba(255, 0, 0, 0.281);
+  flex-direction: column;
+
+  .content-box {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+
+  &_wrapper {
+    display: flex;
+    width: 100%;
+    height: 300px;
+    // background: rgba(0, 128, 0, 0.418);
+
+    &__title {
+      font-size: 20px;
+      font-weight: 700;
+      color: #000;
+    }
+  }
+}
+</style>
